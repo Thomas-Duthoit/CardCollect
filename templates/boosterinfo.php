f63fe8333a7ff2bc065d652c6769b5591ff7ad06
Blackun1 <146175655+Blackun1@users.noreply.github.com>
2024-05-28 18:40:14 +0200

    BoosterInfo
    Informations des boosters corrigés dans la vue d'achat

--- templates/boosterinfo.php
+++ templates/boosterinfo.php
@@ -12,16 +12,22 @@ if (!valider("connected", "SESSION")) {
 }
 
 $booster = infoBooster(valider("idBooster", "GET"));
-
-tprint($booster);
 echo "<div style=\"margin-left: 10px;\">";
     echo "<h1>".$booster[0]["name"]." : " . $booster[0]["cost"] . " Coins </h1>";
     echo "<hr />";
+    echo "<div>";
+    echo "<div class=\"boosterContent r1\">" . "Nombre commun : " . $booster[0]["nbCommon"] . "</div>";
+    echo "<div class=\"boosterContent r2\">" . "Nombre non-commun : " . $booster[0]["nbUncommon"] . "</div>";
+    echo "<div class=\"boosterContent r3\">" . "Nombre epic : " . $booster[0]["nbEpic"] . "</div>";
+    echo "<div class=\"boosterContent r4\">" . "Nombre légendaire : " . $booster[0]["nbLegendary"] . "</div>";
+    echo "<div class=\"boosterContent random\">" . "Nombre random : " . $booster[0]["nbRandom"] . "</div>";
+    echo "</div>";
     echo "<hr />";
     mkForm();
         mkInput("submit", "action", "Acheter booster", "class=\"crimson_button\"");
         mkInput("hidden", "idBooster", $booster[0]["id"]);
         mkInput("hidden", "cost", $booster[0]["cost"]);
+        mkInput("hidden", "view", "shop");
     endForm();
 echo "</div>";
 ?>
