27324d78dca3321d38e8f7f8ac6e4766fe9f336b
Blackun1 <146175655+Blackun1@users.noreply.github.com>
2024-06-01 16:06:27 +0200

    Popups
    Popups pour les détails d'une carte. Possibilité de DL

--- libs/config.php
+++ libs/config.php
@@ -2,8 +2,8 @@
 
 // MACHINE LINUX 
 $BDD_host="localhost";
-$BDD_user="thomas";
-$BDD_password="100206"; // vide sous windows
+$BDD_user="admin";
+$BDD_password="mysql"; // vide sous windows
 $BDD_base="CardCollect";
 
 ?>
--- templates/cardInventory.php
+++ templates/cardInventory.php
@@ -21,8 +21,48 @@ echo "</div>";
 
 echo "<hr />";
 echo "<div class=\"booster_inv\">";
-for ($i=0; $i < count($cardInv) ; $i++) { 
+for ($i=0; $i < count($cardInv) ; $i++) {
+	echo "<div class=\"popup_trigger\" onclick=\"popup_open(".$i.")\">";
     mkCard($cardInv[$i]["rarity"], $cardInv[$i]["minia_path"],$cardInv[$i]["name"]);
+	echo "</div>";
 }
 echo "</div>";
 ?>
+
+<!--- POPUP --->
+<div id="popup">
+	<img src="ressources/cross_icon.png" alt="cross icon" id="popup_cross" onclick="popup_close()" />
+		<div id="popup_content">
+		</div>
+		<div id="popup_options">
+		</div>
+</div>
+
+<script>
+
+		cards_infos = <?php echo json_encode($cardInv)?>
+
+		function popup_open(i) {
+			console.log("fonction popup_open");
+			console.log(cards_infos[i]);
+			popup = document.getElementById("popup");
+			popup_content = document.getElementById("popup_content");
+			popup_options = document.getElementById("popup_options");
+
+			popup_content.innerHTML = "<img src=img/"+cards_infos[i]["poster_path"]+" alt=\"popup poster carte\" id=\"popup_poster\">";
+			popup_content.innerHTML = popup_content.innerHTML + "<h2>"+ cards_infos[i]["name"]+"</h2>";
+			popup_content.innerHTML = popup_content.innerHTML + "<p>"+ cards_infos[i]["description"]+"<p>";
+
+			popup_options.innerHTML = "<a href=\"img/"+cards_infos[i]["poster_path"]+"\" download=\""+cards_infos[i]["name"]+"\">" +
+									  "<img src=\"ressources/download_icon.png\" alt=\"download_icon\" id=\"popup_download\">";
+		
+			popup.style.display = "block";
+		}
+
+		function popup_close() {
+			console.log("fonction popup_close");
+			popup = document.getElementById("popup");
+			popup.style.display = "none";
+		}
+
+</script>
